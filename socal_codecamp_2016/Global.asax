638d927a51b82c0f31f623ca6a20cbb069e5f5cb
glitch <glitch@DESKTOP-1JQ2A1R>
2016-11-11 16:48:12 -0800

    Add project files.

---
+++ socal_codecamp_2016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="socal_codecamp_2016.MvcApplication" Language="C#" %>
---
+++ socal_codecamp_2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(socal_codecamp_2016.Startup))]
+namespace socal_codecamp_2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
